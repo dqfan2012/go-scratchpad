1e3bd39b9944716121ff9b04c8d83d9e260068cd
Samuel Stidham <dqfan2012@gmail.com>
2024-05-19 02:44:31 -0400

    Fixing the LinkedList interface

--- internal/datastructures/linkedlist/list.go
+++ internal/datastructures/linkedlist/list.go
@@ -3,7 +3,7 @@ package linkedlist
 // List interface defines the operations for a singly linked list.
 type LinkedList interface {
 	ClearList()
-	DeleteAtPosition(int) (int, bool)
+	DeleteAtPosition(int) (int, bool, error)
 	DeleteHead() (int, bool)
 	DeleteTail() (int, bool)
 	IsEmpty() bool
